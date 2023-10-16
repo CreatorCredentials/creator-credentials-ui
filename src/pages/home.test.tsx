f707363ec68d373e0c3df924ca66a1abf4a2beff
Kamil Dubiel <kamil.dubiel@leocode.com>
2023-10-16 18:59:43 +0200

    feat: Configure internationalization

--- src/pages/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import Home from './index';
-
-test('App: Renders correctly', () => {
-  render(<Home />);
-  expect(screen.getByText('Hello world!')).toBeInTheDocument();
-});
