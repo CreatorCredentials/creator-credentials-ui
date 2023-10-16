3b24b36931b9f3f658b84d7d8562481ab8f86f6a
Kamil Dubiel <kamil.dubiel@leocode.com>
2023-10-16 22:45:03 +0200

    chore: Add MSW for development and tests

--- src/components/modules/helloWorld/HelloWorld.tsx
+++ src/components/modules/helloWorld/HelloWorld.tsx
@@ -1,8 +1,36 @@
 import { useTranslation } from 'next-i18next';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { axios } from '@/shared/utils/axios';
 
 export const HelloWorld = () => {
   const { t } = useTranslation('common');
+  const [getData, setGetData] = useState(null);
+  const [postData, setPostData] = useState(null);
 
-  return <div>{t('hello-world')}</div>;
+  const handleButtonClick = async () => {
+    const response = await axios.post('/hello-world', {
+      hello: 'world',
+    });
+    setPostData(response.data);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/hello-world');
+        setGetData(response.data);
+      } catch (error) {
+        console.error('Could not fetch ....');
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>{t('hello-world')}</button>
+      {getData && <div>{getData}</div>}
+      {postData && <div>{postData}</div>}
+    </div>
+  );
 };
