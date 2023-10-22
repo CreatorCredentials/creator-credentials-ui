edfae7a5a95c14c4ed9866cd1ab34d6d6dd2f030
Kamil Dubiel <kamil.dubiel@leocode.com>
2023-10-22 23:19:10 +0200

    feat: Change app module setup, delete hello world code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'globals.css';
 import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'globals.css';
