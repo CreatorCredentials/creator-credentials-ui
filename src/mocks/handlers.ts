ace2a239687b2dbed4072f6a9e633852fc3e015f
Kamil Dubiel <kamil.dubiel@leocode.com>
2023-10-22 23:29:02 +0200

    feat: Add auth and signup domains to MSW

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,4 @@
-import { rest } from 'msw';
-import {
-  mockGetHelloWorld,
-  mockPostHelloWorld,
-} from './resolvers/mockHelloWorld';
-import { MOCK_API_URL } from './config';
+import { authHandlers } from './domains/auth';
+import { signupHandlers } from './domains/signup';
 
-export const handlers = [
-  rest.get(`${MOCK_API_URL}/hello-world`, mockGetHelloWorld),
-  rest.post(`${MOCK_API_URL}/hello-world`, mockPostHelloWorld),
-];
+export const handlers = [...signupHandlers, ...authHandlers];
