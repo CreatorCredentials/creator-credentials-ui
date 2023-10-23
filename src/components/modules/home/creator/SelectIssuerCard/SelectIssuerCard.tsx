f374467eb61ba87f9e685780cf5f96989ddda23f
Kamil Dubiel <kamil.dubiel@leocode.com>
2023-10-23 16:30:45 +0200

    feat: Add Creators Home Page

---
+++ src/components/modules/home/creator/SelectIssuerCard/SelectIssuerCard.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from 'next-i18next';
+import { Button } from 'flowbite-react';
+import Link from 'next/link';
+import { ElementType } from 'react';
+import { HomeCard } from '@/components/modules/home/HomeCard';
+
+export const SelectIssuerCard = () => {
+  const { t } = useTranslation('home-creator');
+
+  return (
+    <HomeCard
+      title={t('select-issuer.title')}
+      badge={{
+        iconName: 'Building',
+        color: 'primary',
+        label: t('select-issuer.badge'),
+      }}
+      renderFooter={({ icon }) => (
+        <Button
+          color="primary"
+          href="/issuers"
+          as={Link as ElementType}
+          className="inline"
+        >
+          {t('select', { ns: 'common' })}
+          {icon}
+        </Button>
+      )}
+    >
+      <p className="text-lg text-grey-4">{t('select-issuer.subtitle')}</p>
+    </HomeCard>
+  );
+};
