757df414f6e757b66b1a27abb7609e8958f61dab
yurykharytanovich <yurykharytanovich@gmail.com>
2024-01-17 15:30:49 +0100

    remove get initial props for mocked data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Flowbite } from 'flowbite-react';
 import { appWithTranslation } from 'next-i18next';
-import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { useState } from 'react';
 // eslint-disable-next-line no-restricted-imports
@@ -12,8 +12,6 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { createQueryClient } from '@/shared/utils/queryClient';
 import { clsxm } from '@/shared/utils/clsxm';
 import { NextPageWithLayout } from '@/shared/typings/NextPageWithLayout';
-import { config } from '@/shared/constants/config';
-import { initMocks } from '@/mocks/index';
 import { flowbiteTheme } from '@/components/flowbite.theme';
 import { SidebarLayout } from '@/components/layouts/sidebarLayout/SidebarLayout';
 import { AppMetadata, MocksProvider } from '@/components/modules/app';
@@ -54,17 +52,4 @@ function CreatorCredentialsApp({ Component, pageProps }: AppPropsWithLayout) {
     </>
   );
 }
-
-CreatorCredentialsApp.getInitialProps = async (
-  context: AppContext,
-): Promise<AppInitialProps> => {
-  const ctx = await App.getInitialProps(context);
-
-  if (config.API_MOCKING === 'enabled') {
-    await initMocks();
-  }
-
-  return { ...ctx };
-};
-
 export default appWithTranslation(CreatorCredentialsApp);
