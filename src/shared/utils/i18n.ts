a51e8d9946046a0bd556cbbe83a1ccd623f12dd4
yurykharytanovich <yurykharytanovich@gmail.com>
2024-01-17 23:08:20 +0100

    disable linter for logs

--- src/shared/utils/i18n.ts
+++ src/shared/utils/i18n.ts
@@ -4,7 +4,9 @@ export const getI18nProps = async (
   locale: string | undefined,
   ns: string[] = [],
 ) => {
+  // eslint-disable-next-line
   console.log('getI18nProps called');
+  // eslint-disable-next-line
   console.log('getI18nProps locale: ', locale);
   if (!locale) {
     throw new Error('no locale getI18nProps exception');
