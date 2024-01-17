65c9380ff83928b02ccf4dd5d6d6152eb2808b41
yurykharytanovich <yurykharytanovich@gmail.com>
2024-01-17 23:03:49 +0100

    add logs for testing

--- src/shared/utils/i18n.ts
+++ src/shared/utils/i18n.ts
@@ -3,6 +3,13 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export const getI18nProps = async (
   locale: string | undefined,
   ns: string[] = [],
-) => ({
-  ...(locale && (await serverSideTranslations(locale, ['common', ...ns]))),
-});
+) => {
+  console.log('getI18nProps called');
+  console.log('getI18nProps locale: ', locale);
+  if (!locale) {
+    throw new Error('no locale getI18nProps exception');
+  }
+  return {
+    ...(locale && (await serverSideTranslations(locale, ['common', ...ns]))),
+  };
+};
