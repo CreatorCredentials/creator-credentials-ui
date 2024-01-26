bce116c627e050683ac614762822640367805831
yurykharytanovich <yurykharytanovich@gmail.com>
2024-01-26 16:30:19 +0100

    add logs for translations

--- src/shared/utils/i18n.ts
+++ src/shared/utils/i18n.ts
@@ -4,7 +4,19 @@ export const getI18nProps = async (
   locale: string | undefined,
   ns: string[] = [],
 ) => {
+  // eslint-disable-next-line
+  console.log('asdf asdf getI18nProps local: ', locale);
+  // eslint-disable-next-line
+  console.log('asdf asdf getI18nProps ns: ', ns);
+
+  const result = await serverSideTranslations(locale || 'en', [
+    'common',
+    ...ns,
+  ]);
+  // eslint-disable-next-line
+  console.log('asdf asdf getI18nProps result: ', result);
+
   return {
-    ...(await serverSideTranslations(locale || 'en', ['common', ...ns])),
+    ...result,
   };
 };
