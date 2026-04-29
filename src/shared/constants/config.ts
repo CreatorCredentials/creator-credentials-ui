790611246774502b2a63c1ee8ef5d6c84d7fe71f
yurykharytanovich <yurykharytanovich@gmail.com>
2026-04-29 16:53:56 +0200

    fix env in config

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -9,11 +9,10 @@ const readRequiredEnv = (key: string) => {
 };
 
 export const config = {
-  API_URL: readRequiredEnv('API_URL'),
   API_MOCKING: process.env.API_MOCKING || 'disabled',
   TERMS_AND_CONDITIONS_URL: 'https://creatorcredentials.com',
   NEST_API_URL: readRequiredEnv('NEST_API_URL'),
   NEST_API_SSR_URL: readRequiredEnv('NEST_API_SSR_URL'),
-  NEXTAUTH_URL: readRequiredEnv('NEXTAUTH_URL'),
+  // NEXTAUTH_URL: readRequiredEnv('NEXTAUTH_URL'),
   DISABLE_I18N_TRANSLATIONS: process.env.DISABLE_I18N_TRANSLATIONS === 'true',
 };
